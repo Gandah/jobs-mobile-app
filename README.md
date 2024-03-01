697273eaec4afae82257d557da58b4549f8e9d55
ganderson <gandahkelvin33@gmail.com>
2024-03-01 12:26:38 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# #️⃣Mobile Project - Job Search App
+
+## Table of contents
+
+- [#️⃣Mobile Project - Job Search App](#️⃣mobile-project---job-search-app)
+  - [Table of contents](#table-of-contents)
+  - [Overview](#overview)
+  - [🛠️Core Features](#️core-features)
+    - [📸Screenshots](#screenshots)
+    - [🔗Links](#links)
+    - [⚙️Built with](#️built-with)
+    - [👨‍💻Continued development](#continued-development)
+  - [Author](#author)
+
+## Overview
+
+A mobile application for job search with a visually pleasing interface.
+
+## 🛠️Core Features
+
+▶️ Dynamic Home page.
+
+▶️ Third Party API integration.
+
+▶️ Browse and search for various jobs across the global.
+
+▶️ Search & Pagination Functionality.
+
+▶️ Apply to your dream Job.
+
+### 📸Screenshots
+
+![Home](./screenshots/home.jpg)
+
+### 🔗Links
+
+- GitHub Repo: [View Repo](https://github.com/Gandah/jobs-mobile-app.git)
+
+### ⚙️Built with
+
+- Node.js
+- React Native
+- Axios
+- Expo
+- JSearch API
+
+### 👨‍💻Continued development
+
+- Add skeleton loaders
+
+## Author
+
+- LinkedIn - [MyLinkedIn](https://www.linkedin.com/in/gandahkelvin)
+- Website - [Portfolio](https://gandah-porfolio.vercel.app/)
+- Twitter - [@mr_g4nderson](https://twitter.com/mr_g4nderson?t=A5NobjZab2sVEdh3Zq9s0A&s=09)
---
+++ components/home/welcome/Welcome.jsx
@@ -0,0 +1,70 @@
+import { useState } from 'react'
+import { useRouter } from 'expo-router'
+import { View, 
+  Text, 
+  TextInput, 
+  Image,
+  TouchableOpacity, 
+  FlatList} from 'react-native'
+
+import styles from './welcome.style'
+import { icons, SIZES } from "../../../constants"
+
+const jobTypes = ["Full-time", "Part-time", "Contract"]
+
+const Welcome = ({ searchTerm, setSearchTerm, handlePress}) => {
+  const router = useRouter();
+
+  const [activeJobType, setActiveJobType] = useState("Full-time")
+  
+  return (
+    <View>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Welcome Anna</Text>
+        <Text style={styles.welcomeMessage}>Find your dream job</Text>
+      </View>
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput style={styles.searchInput} 
+            value={searchTerm}
+            onChangeText={(text) => setSearchTerm(text)}
+            placeholder="Search Jobs"
+          />
+        </View>
+
+        <TouchableOpacity 
+          style={styles.searchBtn}
+          onPress={handlePress}
+        >
+          <Image 
+          source={icons.search}
+            resizeMode="contain"
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.tabsContainer}>
+        <FlatList 
+          data={jobTypes}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={styles.tab(activeJobType, item)}
+              onPress={() => {
+                setActiveJobType(item)
+                router.push(`/search/${item}`)
+              }}
+            >
+              <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
+            </TouchableOpacity>
+          )}
+          keyExtractor={item => item}
+          contentContainerStyle={{ columnGap: SIZES.small}}
+          horizontal
+        />
+      </View>
+    </View>
+  )
+}
+
+export default Welcome
